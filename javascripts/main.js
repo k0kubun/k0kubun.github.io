7c7a12b90fb89e2847e28196055e7f21eaf9658f
Takashi Kokubun <takashikkbn@gmail.com>
2014-05-20 20:52:02 +0900

    Add recent works

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,43 @@
-console.log('This would be the main JS file.');
+$(function(){
+  $.ajax({
+    type: 'GET',
+    url: 'https://api.github.com/users/k0kubun/repos?sort=created',
+    cache: false,
+    dataType: 'jsonp',
+    success: function(data) {
+      var datas, i, l, recentWork, recentWorks, desc;
+      datas = data.data;
+      recentWorks = $('ul.recent-works');
+
+      l = datas.length;
+      if (l > 10) {
+        l = 10;
+      }
+
+      for (i = 0; i < l; i++) {
+        data = datas[i];
+        recentWork = $('<li><a class="url"></a><span class="desc" /></li>');
+        recentWork.find('a').attr('href', data.html_url).html(data.name);
+
+        desc = (data.description || '').slice(0, 60);
+        if (desc != (data.description || '')) {
+          desc += '...';
+        }
+        if (desc.length > 0) {
+          desc = '- ' + desc;
+        }
+        recentWork.find('.desc').text(desc);
+
+        if (data.fork) {
+          recentWork.addClass('fork');
+        }
+
+        if (data.language) {
+          recentWork.addClass('lang-' + data.language.toLowerCase());
+        }
+
+        recentWorks.append(recentWork);
+      }
+    }
+  });
+});
